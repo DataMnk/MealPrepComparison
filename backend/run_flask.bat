f7783ea2f6ccdd2417a37343486264b3abb51ead
root <Jikiyama@gmail.com>
2025-03-17 19:52:25 -0400

    hola diana :)

--- backend/run_flask.bat
+++ backend/run_flask.bat
@@ -4,7 +4,7 @@ echo Starting Flask server...
 REM Check if venv exists, if not create it
 if not exist venv (
     echo Creating virtual environment...
-    python -m venv venv
+    python3106 -m venv venv
 )
 
 REM Activate the virtual environment
@@ -16,7 +16,7 @@ pip install -r requirements.txt
 
 REM Run the Flask application
 echo Starting server...
-python app.py
+python3106 app.py
 
 REM Deactivate the virtual environment when the server is stopped
 call venv\Scripts\deactivate 
